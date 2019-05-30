8ca04883013db3c71944802a3c807f49d1ab33cb
Marty Spiewak <mspiewak@pivotal.io>
2019-05-30 13:54:47 -0400

    Remove pivotal team
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- credhub.go
+++ credhub.go
@@ -1,32 +1,15 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/dpb587/go-slack-topic-bot/message"
 	"github.com/dpb587/go-slack-topic-bot/message/pairist"
 	"github.com/martys34/go-slack-topic-bot/slack"
-	"log"
-	"os"
-	"strings"
-	"time"
 )
 
 func main() {
-	pivotalTeam := pairist.PeopleInRole{
-		Team: "therealslimcredhub",
-		Role: "Bat-person",
-		People: map[string]string{
-			"Andrew":   "U8RH9A30R",
-			"Mark":     "U05661PTK",
-			"Marty":    "UBXEUQN6P",
-			"Josh":     "U1V6C2UKW",
-			"Tom":      "UG262F4EB",
-			"Victoria": "U6SUTRCKB",
-		},
-	}
-	pivotalPM := []string {"<@U0DFF9JLB>", "<@U27926NR3>"}
-	createChannelMessage("PIVOTAL",
-		"see pinned messages for helpful links", pivotalTeam, pivotalPM)
-
 	cloudFoundryTeam := pairist.PeopleInRole{
 		Team: "therealslimcredhub",
 		Role: "Bat-person",
@@ -40,39 +23,18 @@ func main() {
 		},
 	}
 
-	cloudFoundryPM := []string {"<@UDFK4K0KT>", "<@UHPMJCXGC>"}
-	createChannelMessage("CLOUDFOUNDRY",
-		"Please include your CredHub logs in case of Errors", cloudFoundryTeam, cloudFoundryPM)
-
-	interruptPair, _ := pivotalTeam.Message()
-	githubReminder := "gentle reminder to check GitHub issues 😊"
-	err := slack.SendMessage(interruptPair, githubReminder)
-
-	if time.Now().Weekday() == time.Weekday(5) {
-		err = slack.SendMessage(interruptPair,
-			"don't forget to spin the feedback wheel! :fidgetspinner:\nhttps://tinyurl.com/credhubfeedback")
-	}
-
-	if err != nil {
-		log.Panicf("ERROR: %v", err)
-	}
-}
-
-func createChannelMessage(workspace, firstLine string, team pairist.PeopleInRole, PM []string) {
-	pmString := strings.Join(PM, ", ")
-
 	msg, err := message.Join(
 		" | ",
-		message.Literal(firstLine),
+		message.Literal("Please include your CredHub logs in case of Errors"),
 		message.Prefix(
 			"interrupt: ",
 			message.Conditional(
 				pairist.WorkingHours("09:00", "18:00", "America/New_York"),
 				message.Join(
 					" ",
-					team,
+					cloudFoundryTeam,
 					message.Literal("| break glass: `@credhub-team` |"),
-					message.Literal("PM: " + pmString),
+					message.Literal("PM: <@UDFK4K0KT>, <@UHPMJCXGC>"),
 				),
 			),
 		),
@@ -84,11 +46,8 @@ func createChannelMessage(workspace, firstLine string, team pairist.PeopleInRole
 
 	log.Printf("DEBUG: expected message: %s", msg)
 
-	err = slack.UpdateChannelTopic(os.Getenv(workspace + "_SLACK_CHANNEL"), os.Getenv(workspace + "_SLACK_TOKEN"), msg)
+	err = slack.UpdateChannelTopic(os.Getenv("CLOUDFOUNDRY_SLACK_CHANNEL"), os.Getenv("CLOUDFOUNDRY_SLACK_TOKEN"), msg)
 	if err != nil {
 		log.Panicf("ERROR: %v", err)
 	}
-
 }
-
-
